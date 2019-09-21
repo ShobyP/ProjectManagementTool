3bb7e1accf9823c06242ecf0be89c78e0e7a228b
shoaib <main.shoby@gmail.com>
2019-09-21 16:56:50 -0400

    Project Update Functionality - WIP 2
    Update process works, need to handle exceptions.

--- project-management-tool-frontend/src/actions/projectActions.js
+++ project-management-tool-frontend/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
@@ -19,3 +19,13 @@ export const getProjects = () => async dispatch => {
     payload: res.data
   });
 };
+
+export const getProject = (id, history) => async dispatch => {
+  const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+  //history.push("/dashboard");
+  dispatch({
+    type: GET_PROJECT,
+    payload: res.data
+  });
+};
+
