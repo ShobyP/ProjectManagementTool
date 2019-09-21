202434a8860f451e846d16dc70044ceeeaf0cf65
shobyp <N3wcode123>
2019-09-21 13:40:35 -0400

    redux store wip 2nd half.

--- project-management-tool-frontend/src/components/Dashboard.js
+++ project-management-tool-frontend/src/components/Dashboard.js
@@ -4,8 +4,16 @@
 import React, { Component } from "react";
 import ProjectItem from "./Project/ProjectItem";
 import CreateProjectButton from "./Project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from 'prop-types';
 
 class Dashboard extends Component {
+
+  componentDidMount(){
+    this.props.getProjects();
+  }
+
   render() {
     return (
       <div className="projects">
@@ -13,8 +21,8 @@ class Dashboard extends Component {
           <div className="row">
             <div className="col-md-12">
               <h1 className="display-4 text-center">Projects</h1>
-              <br />        
-              <CreateProjectButton/>
+              <br />
+              <CreateProjectButton />
               <br />
               <hr />
               <ProjectItem />
@@ -26,4 +34,16 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+Dashboard.propType = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  project: state.project
+});
+
+export default connect(
+  mapStateToProps,
+  { getProjects }
+)(Dashboard);
