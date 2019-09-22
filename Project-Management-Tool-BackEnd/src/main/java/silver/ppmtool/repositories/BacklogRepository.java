980314a8b0c12f96e65c6992a5f203012f2ea2d7
ShobyP <main.shoby@gmail.com>
2019-09-21 23:06:29 -0400

    BackEnd - Created new entities for project backlog and Tasks.

---
+++ Project-Management-Tool-BackEnd/src/main/java/silver/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package silver.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import silver.ppmtool.domain.Backlog;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+}
