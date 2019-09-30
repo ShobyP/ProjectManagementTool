d6829975aa223e28483e117cbd9fd77c527492dc
shobyp <N3wcode123>
2019-09-30 19:41:59 -0400

    folder layout for project task done.

--- project-management-tool-frontend/src/actions/projectActions.js
+++ project-management-tool-frontend/src/actions/projectActions.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
+import {
+  GET_ERRORS,
+  GET_PROJECTS,
+  GET_PROJECT,
+  DELETE_PROJECT,
+  //DELETE_PROJECT_TASK
+} from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
